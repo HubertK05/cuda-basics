a60dee0588138b6a0fb8e489a9265f1677f57ba2
HubertK05 <hubert.klimowicz.hk@gmail.com>
2025-01-20 22:00:40 +0100

    Ignore compiled files

---
+++ test_app.cu
@@ -0,0 +1,17 @@
+// Including Libraries
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+// Defining the CUDA Kernel
+// __global__ void vectorAdd(int *a, int *b, int *c, int n) {
+//     int i = threadIdx.x;
+//     if (i < n) {
+//         c[i] = a[i] + b[i];
+//     }
+// }
+
+
+// Main Function
+int main() {
+    return 0;
+}
