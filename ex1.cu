675e208a0d5813862d1fd4eadc46d131682f82a1
HubertK05 <hubert.klimowicz.hk@gmail.com>
2025-01-21 21:22:58 +0100

    Complete exercise 1

---
+++ ex1.cu
@@ -0,0 +1,44 @@
+// Including Libraries
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+// Defining the CUDA Kernel
+__global__ void vectorAdd(int *a, int *out, int n) {
+    int i = threadIdx.x;
+    if (i < n) {
+        out[i] = a[i] + 10;
+    }
+}
+
+
+// Main Function
+int main() {
+    const int n = 256;
+    int a[n], out[n];
+    int *d_a, *d_out;
+
+    // Allocate device memory
+    cudaMalloc((void**)&d_a, n * sizeof(int));
+    cudaMalloc((void**)&d_out, n * sizeof(int));
+
+    // Initialize arrays and copy to device
+    for(int i = 0; i < n; i++) {
+        a[i] = i;
+    }
+    cudaMemcpy(d_a, a, n * sizeof(int), cudaMemcpyHostToDevice);
+
+    // Launch kernel
+    vectorAdd<<<1, n>>>(d_a, d_out, n);
+
+    // Copy result back to host
+    cudaMemcpy(out, d_out, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+    // Cleanup
+    cudaFree(d_a); cudaFree(d_out);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d + 10 = %d\n", a[i], out[i]);
+    }
+
+    return 0;
+}
