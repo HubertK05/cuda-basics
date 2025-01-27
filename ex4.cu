dcd2b1f02ae2233c70985c4883f9db18d9083c09
HubertK05 <hubert.klimowicz.hk@gmail.com>
2025-01-27 21:34:24 +0100

    Complete exercise 4

--- ex4.cu
+++ ex4.cu
@@ -4,10 +4,10 @@
 
 // Defining the CUDA Kernel
 __global__ void vectorAdd(int **a, int **out, int n) {
-    int i = threadIdx.x;
-    printf("%d ", a[0][i]);
-    if (i < n) {
-        out[0][i] = a[0][i] + 10;
+    int x = threadIdx.x / n;
+    int y = threadIdx.x % n;
+    if (x < n) {
+        out[x][y] = a[x][y] + 10;
     }
 }
 
@@ -15,45 +15,35 @@ __global__ void vectorAdd(int **a, int **out, int n) {
 // Main Function
 int main() {
     const int n = 4;
+
     int a[n][n], out[n][n];
-    int **d_a, **d_out, *d_a_rows[n], *d_out_rows[n];
-
-    // Allocate device memory
-    printf("Doing main arrays\n");
-    cudaMalloc((void**)&d_a, n * sizeof(int *));
-    cudaMalloc((void**)&d_out, n * sizeof(int *));
-    printf("Main arrays done\n");
     for (int i = 0; i < n; i++) {
-        printf("Subarray %d\n", i);
-        cudaMalloc((void**)&d_a_rows[i], n * sizeof(int));
-        cudaMalloc((void**)&d_out_rows[i], n * sizeof(int));
-    }
-
-    // Initialize arrays and copy to device
-    printf("Initializing arrays\n");
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             a[i][j] = i * n + j;
         }
     }
-    printf("Copying memory\n");
-    cudaMemcpy(d_a, d_a_rows, n * sizeof(int *), cudaMemcpyHostToDevice);
 
-    // Launch kernel
-    printf("Running kernel\n");
-    vectorAdd<<<1, (n + 1) * (n + 1)>>>(d_a, d_out, n);
-
-    // Copy result back to host
-    printf("\nCopying results\n");
-    cudaMemcpy(d_out_rows, d_out, n * sizeof(int *), cudaMemcpyDeviceToHost);
-
-    printf("Cleaning up\n");
-    // Cleanup
+    int **a_ptr, **out_ptr;
+    int *a_rows[n], *out_rows[n];
+    cudaMalloc((void**)&a_ptr, n * sizeof(int*));
+    cudaMalloc((void**)&out_ptr, n * sizeof(int*));
     for (int i = 0; i < n; i++) {
-        cudaFree(d_a_rows[i]);
-        cudaFree(d_out_rows[i]);
+        cudaMalloc((void**)&a_rows[i], n * sizeof(int));
+        cudaMalloc((void**)&out_rows[i], n * sizeof(int));
     }
-    cudaFree(d_a); cudaFree(d_out);
+
+    cudaMemcpy(a_ptr, a_rows, n * sizeof(int*), cudaMemcpyHostToDevice);
+    cudaMemcpy(out_ptr, out_rows, n * sizeof(int*), cudaMemcpyHostToDevice);
+    for (int i = 0; i < n; i++) {
+        cudaMemcpy(a_rows[i], a[i], n * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(out_rows[i], out[i], n * sizeof(int), cudaMemcpyHostToDevice);
+    }
+
+    vectorAdd<<<1, (n + 1) * (n + 1)>>>(a_ptr, out_ptr, n);
+
+    for (int i = 0; i < n; i++) {
+        cudaMemcpy(out[i], out_rows[i], n * sizeof(int), cudaMemcpyDeviceToHost);
+    }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
